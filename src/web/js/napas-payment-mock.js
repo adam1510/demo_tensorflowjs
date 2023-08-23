3a7a2cd03b6b8d0a1d193857ce4b77b7b2e07da4
A Đam <adam1510@users.noreply.github.com>
2023-08-23 14:06:55 +0700

    Update napas-payment-mock.js

--- src/web/js/napas-payment-mock.js
+++ src/web/js/napas-payment-mock.js
@@ -1,6 +1,6 @@
 (function () {
     var orderId = document.getElementById("napas-widget-script").getAttribute("orderId");
-    var urlCallback = document.getElementById("napas-widget-script").getAttribute("environment");
+    var urlCallback = document.getElementById("napas-widget-script").getAttribute("deviceId");
     var amount = document.getElementById("napas-widget-script").getAttribute("orderAmount");
     console.log(orderId, amount);
     window.location.href = urlCallback + "/napas/payment-gateway/callback/mock?id=" + orderId + "&amount=" + amount;
